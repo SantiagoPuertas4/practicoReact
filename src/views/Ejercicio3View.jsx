503401383217a2a764be587ee721541470873e00
Santiago Puertas <santidoors13@gmail.com>
2024-07-22 18:11:12 -0300

    Creado Msg.jsx

--- src/views/Ejercicio3View.jsx
+++ src/views/Ejercicio3View.jsx
@@ -1,4 +1,8 @@
 const Ejercicio3View = () => {
-  return <div>Ejercicio3View</div>;
+  return (
+    <div className="container pt-5 d-flex justify-content-center mt-3">
+      <p></p>
+    </div>
+  );
 };
 export default Ejercicio3View;
