29e4f592d6779a74419b2425f3938b2223e0a1b3
Santiago Puertas <santidoors13@gmail.com>
2024-07-22 18:20:30 -0300

    Creado el mensaje con useState

--- src/components/Msg/Msg.jsx
+++ src/components/Msg/Msg.jsx
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
+// import { useState } from "react";
 const Msg = (props) => {
   const { mensaje } = props;
+
+  // const [mensaje, setmensaje] = useState("");
 
   return (
     <div className="container pt-5 d-flex justify-content-center mt-3">
