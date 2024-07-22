25f3d28d9a9bf695a2918a790cc8433c73839ed2
Santiago Puertas <santidoors13@gmail.com>
2024-07-22 18:29:04 -0300

    Terminado ejercicio3

---
+++ src/components/Common/Ejercicio3/Msg/Msg.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+const Msg = (props) => {
+  const { mensaje } = props;
+
+  const [msgCompleto, setmsgCompleto] = useState(mensaje);
+
+  const handleChange = () => {
+    setmsgCompleto((anterior) => anterior + " (from changes state)");
+  };
+
+  return (
+    <div className="container pt-5 d-flex flex-column  align-items-center justify-content-center mt-3">
+      <h1>Hola {msgCompleto}!</h1>
+      <button className="btn btn-primary" onClick={handleChange}>
+        Cliqueame (useState)
+      </button>
+    </div>
+  );
+};
+export default Msg;
+
+Msg.propTypes = {
+  mensaje: PropTypes.array.isRequired,
+};
--- src/views/Ejercicio3View.jsx
+++ src/views/Ejercicio3View.jsx
@@ -1,7 +1,9 @@
+import Msg from "../components/Common/Ejercicio3/Msg/Msg";
+
 const Ejercicio3View = () => {
   return (
     <div className="container pt-5 d-flex justify-content-center mt-3">
-      <p></p>
+      <Msg />
     </div>
   );
 };
