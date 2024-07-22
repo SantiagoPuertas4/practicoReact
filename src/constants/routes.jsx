5f918c8ef4a8dde2be5556126930e1d6debe4862
Santiago Puertas <santidoors13@gmail.com>
2024-07-22 18:09:41 -0300

    Creada rama del ejercicio 3

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import RootView from "../views/RootView";
 import E1View from "../views/Ejercicio1View";
 import MainView from "../views/MainView";
+import Ejercicio3View from "../views/Ejercicio3View";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "ejercicio1",
         element: <E1View />,
+      },
+      {
+        path: "ejercicio3",
+        element: <Ejercicio3View />,
       },
     ],
   },
---
+++ src/views/Ejercicio3View.jsx
@@ -0,0 +1,4 @@
+const Ejercicio3View = () => {
+  return <div>Ejercicio3View</div>;
+};
+export default Ejercicio3View;
