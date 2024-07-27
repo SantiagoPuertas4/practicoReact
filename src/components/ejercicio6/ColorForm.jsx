b7db5c3cab484acc2906cca0c976604fc673a194
Santiago Puertas <santidoors13@gmail.com>
2024-07-27 04:34:15 -0300

    Terminado ColorForm.jsx que contiene el input de colores y su respectivo guardado y leido de LS

--- src/components/ejercicio6/ColorForm.jsx
+++ src/components/ejercicio6/ColorForm.jsx
@@ -1,22 +1,54 @@
+import { useRef, useState } from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
+
 const ColorForm = (props) => {
   const { setColors } = props;
 
-  const colorPicker = useRef();
+  const [color, setColor] = useState("#000000");
+
+  const $colorPicker = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setColors((prevColors) => [
+      ...prevColors,
+      {
+        value: color,
+      },
+    ]);
+  };
+
+  const handleChange = () => {
+    setColor($colorPicker.current.value);
   };
 
   return (
-    <div className="container bg-dark-subtle rounded-2 ">
-      <form className="d-flex justify-content-center">
-        <input type="color" ref={colorPicker} />
-        <div>
-          <button type="submit" onClick={handleSubmit} className="btn">
+    <div className="container bg-dark-subtle rounded-2">
+      <h3 className="text-center">Color a agregar</h3>
+      <form
+        onSubmit={handleSubmit}
+        className="d-flex flex-column align-items-center"
+      >
+        <div className="d-flex mb-2">
+          <input
+            className="color-box"
+            type="color"
+            onChange={handleChange}
+            ref={$colorPicker}
+            //El navegador tira un error, preguntar a que se debe
+          />
+          <div className="align-self-center ms-2 bg-white rounded-2">
+            <p className="m-2">{color}</p>
+          </div>
+        </div>
+        <div className="mb-2">
+          <button type="submit" className="btn btn-primary">
             Guardar
           </button>
         </div>
       </form>
+      <div></div>
     </div>
   );
 };
