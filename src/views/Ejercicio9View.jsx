f011c3778b8898014b6af4e9c835663f85e6a32b
Santiago Puertas <santidoors13@gmail.com>
2024-07-31 19:46:58 -0300

    Se termino la estructura inicial del formulario

--- src/views/Ejercicio9View.jsx
+++ src/views/Ejercicio9View.jsx
@@ -1,13 +1,35 @@
 const Ejercicio9View = () => {
   return (
     <div className="mt-5 pt-3 mb-2">
-      <div className="container bg-secondary-emphasis">
+      <div className="container bg-dark-subtle rounded-2">
         <div>
-          <div className="">
-            <h3>Administrador de pacientes de veterinaria</h3>
-          </div>
+          <h2 className="text-center pt-2">
+            Administrador de pacientes de veterinaria
+          </h2>
         </div>
-        <div></div>
+        <hr />
+        <div>
+          <h3 className="text-center">
+            Llenar el formulario para crear una cita
+          </h3>
+        </div>
+        <hr />
+        <form className="" action="">
+          <fieldset>
+            <label className="form-label" htmlFor="">
+              Nombre de la mastoca
+            </label>
+            <input className="form-control" type="text" />
+            <div>
+              <span></span>
+            </div>
+          </fieldset>
+          <div>
+            <button type="submit" className="btn btn-primary">
+              Agendar cita
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
