37a8ea0bc1649744ccc77477bacbd4c7815b3908
Santiago Puertas <santidoors13@gmail.com>
2024-07-31 17:15:05 -0300

    Se creo la ruta al ejercicio 9

---
+++ src/views/Ejercicio9View.jsx
@@ -0,0 +1,15 @@
+const Ejercicio9View = () => {
+  return (
+    <div className="mt-5 pt-3 mb-2">
+      <div className="container bg-secondary-emphasis">
+        <div>
+          <div className="">
+            <h3>Administrador de pacientes de veterinaria</h3>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default Ejercicio9View;
