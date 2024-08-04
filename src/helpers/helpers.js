881bf6b4637ea62612a5ef839b37951613f2a622
Santiago Puertas <santidoors13@gmail.com>
2024-08-04 17:37:14 -0300

    Se cambio la funcion randomAvatar

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,4 +1,4 @@
-export const randomAvatar = (style = "miniavs") => {
+export const randomAvatar = () => {
   const numeroRandom = Math.floor(Math.random() * 1000);
-  return `https://api.dicebear.com/9.x/${style}/svg?seed=${numeroRandom}`;
+  return `https://api.dicebear.com/9.x/miniavs/svg?seed=${numeroRandom}`;
 };
