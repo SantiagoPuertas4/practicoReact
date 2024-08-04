b0c0de40efdd4cc9113e844703745773c532b251
Santiago Puertas <santidoors13@gmail.com>
2024-08-04 18:06:40 -0300

    Se agrego un parrafo en caso de que no hayan colores guardados el LS

--- src/components/ejercicio6/ColorList.jsx
+++ src/components/ejercicio6/ColorList.jsx
@@ -6,6 +6,9 @@ const ColorList = (props) => {
 
   return (
     <div className="container row my-2 d-flex justify-content-center m-0">
+      {colors.length === 0 && (
+        <p className="text-center text-break mt-3">No hay colores guardados</p>
+      )}
       {colors.map((color) => (
         <ColorItem key={color.id} color={color} setColors={setColors} />
       ))}
