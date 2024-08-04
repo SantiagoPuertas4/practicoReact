8dff669aef96b4e5d57218ff7742724f37242891
Santiago Puertas <santidoors13@gmail.com>
2024-08-04 17:35:49 -0300

    Se cambio la estetica de las cards de citas

--- src/components/ejercicio9/Cita.jsx
+++ src/components/ejercicio9/Cita.jsx
@@ -10,16 +10,15 @@ const Cita = (props) => {
   };
 
   return (
-    <div className="col-12 col-lg-3 col-md-4 my-2">
+    <div className="col-12 col-md-4 my-2">
       <div className="card p-2 bg-dark-subtle">
+        <div></div>
         <div className="mt-3 px-2">
-          <p>Nombre: {cita.nombreMascota}</p>
+          <h4 className="m-0">Mascota: {cita.nombreMascota}</h4>
+          <h6>Dueño: {cita.nombreDuenio}</h6>
         </div>
         <hr className="m-0" />
         <div className=" w-100 px-2 my-2">
-          <div>
-            <p>Dueño: {cita.nombreDuenio}</p>
-          </div>
           <div>
             <p>Fecha (Año-Mes-Dia): {cita.fecha}</p>
           </div>
@@ -33,10 +32,10 @@ const Cita = (props) => {
         <hr className="m-0" />
         <div className="d-flex flex-column px-2">
           <button
-            className="btn btn-danger my-3 align-self-end"
+            className="btn btn-danger my-2 align-self-end"
             onClick={handleDelete}
           >
-            Borrar
+            Borrar X
           </button>
         </div>
       </div>
