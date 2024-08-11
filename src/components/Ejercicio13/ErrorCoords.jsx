8b790e83c9c8325009876db652e80a12388a3198
Santiago Puertas <santidoors13@gmail.com>
2024-08-10 22:27:17 -0300

    Terminado ejercicio 13

---
+++ src/components/Ejercicio13/ErrorCoords.jsx
@@ -0,0 +1,8 @@
+const ErrorCoords = () => {
+  return (
+    <div>
+      <h1>La ubicacion ingresada no existe</h1>
+    </div>
+  );
+};
+export default ErrorCoords;
--- src/views/Ejercicio13View.jsx
+++ src/views/Ejercicio13View.jsx
@@ -1,14 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
 import Clima from "../components/Ejercicio13/Clima";
 import Formulario from "../components/Ejercicio13/Formulario";
+import { getCoords, getWeather } from "../api/clima";
 
 const Ejercicio13View = () => {
+  const {
+    data: coords,
+    isError: isErrorCoords,
+    isLoading: isLoadingCoords,
+    isSuccess: isSuccessCoords,
+  } = useQuery({
+    queryKey: ["coords"],
+    queryFn: (query) => getCoords(query),
+    enabled: false,
+    retry: 3,
+  });
+
+  const {
+    data: weather,
+    isError: isErrorWeather,
+    isLoading: isLoadingWeather,
+    isSuccess: isSuccessWeather,
+  } = useQuery({
+    queryKey: ["weather"],
+    queryFn: (query) => getWeather(query),
+    enabled: false,
+    retry: 3,
+  });
+
   return (
-    <div>
+    <div className="mt-5 pt-3 pb-2 mb-2">
       <section>
-        <Formulario />
+        <Formulario isSuccessCoords={isSuccessCoords} coords={coords} />
       </section>
-      <section>
-        <Clima />
+      <section className="d-flex justify-content-center mt-2">
+        <Clima
+          isErrorCoords={isErrorCoords}
+          isLoadingCoords={isLoadingCoords}
+          isSuccessCoords={isSuccessCoords}
+          weather={weather}
+          isErrorWeather={isErrorWeather}
+          isLoadingWeather={isLoadingWeather}
+          isSuccessWeather={isSuccessWeather}
+        />
       </section>
     </div>
   );
